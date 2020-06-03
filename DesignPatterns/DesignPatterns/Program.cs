4232d7e51528125f3aeeaeed0ddcb2b6295dfa37
Sjors <sjors.jessen@movares.nl>
2020-06-03 17:46:56 +0200

    Single Responsibility Principle

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace DesignPatterns
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //Memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+                File.WriteAllText(fileName, journal.ToString());
+        }
+    }
+    
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate a bug");
+            WriteLine(journal);
+            
+            var persistence = new Persistence();
+            const string fileName = @"C:\Users\jesseslgj\Documents\Design Patterns\Design Patterns\DesignPatterns\Temp\journal.txt";
+            persistence.SaveToFile(journal, fileName, true);
+            Process.Start(fileName);
+        }
+    }
+}
